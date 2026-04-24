8c958b5e197c3abb0ecea04396ac9c508155c5be
El Hadji Mamadou Drame Kandji <kandji0987@gmail.com>
2026-04-24 19:57:07 +0000

    Ajout Footer, date Header et composant Statistique

---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+  return (
+    <div className="statistique">
+      <h2 className="stat-chiffre">10</h2>
+      <p className="stat-libelle">Lignes</p>
+    </div>
+  );
+}
+
+export default Statistique;
